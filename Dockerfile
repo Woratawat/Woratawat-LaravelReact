55177abe18121425e145de8d3e1bbcf4e956caf1
Woratawat <kanoon1842@gmail.com>
2022-08-08 20:49:11 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM php:8.0.0-fpm-buster
+
+RUN docker-php-ext-install bcmath pdo_mysql
+
+RUN apt-get update
+RUN apt-get install -y git zip unzip
+
+RUN curl -sL https://deb.nodesource.com/setup_14.x | bash - 
+RUN apt-get install -y nodejs
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Set working directory
+WORKDIR /var/www
+
+EXPOSE 9000
---
+++ src/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Test Redis
+Route::get('/store', function() {
+  Redis::set('Bangkok', 'Krung Thep Maha Nakhon');
+});
+
+Route::get('/retrieve', function() {
+  return Redis::get('Bangkok');
+});
